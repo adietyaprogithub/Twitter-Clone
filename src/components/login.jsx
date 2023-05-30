d09fb6a252bb84a1bbddc17dc8a67609c3703a0c
adietyaprogithub <adityashende9922@gmail.com>
2023-05-30 19:29:48 +0530

    new

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function Login() {
-  return (
-    <div>Log in page</div>
-  )
+export function LoginPage(){
+    return(
+        <>
+        <div>
+        <div>Icon-Twitter</div>
+        <h1>Sign in to Twitter</h1>
+        <div>
+        <button>Sign in with Google</button>
+        </div>
+        <div>
+        <button>Sign in with Apple</button>
+        </div>
+        <span>or</span>
+
+        <input type="text" />
+
+        <div><button>Next</button></div>
+
+        <div><button>Forgot password?</button></div>
+
+        <h3>Don't have an account<Link>Sign up</Link></h3>
+        </div>
+        </>
+    )
 }
