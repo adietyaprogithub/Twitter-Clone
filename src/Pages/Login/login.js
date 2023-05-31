7135a66843c1459c0cfb79c8bb2cc764f8ed1fd4
batman <batman@gmail.com>
2023-05-31 14:57:31 +0530

    This is all updated code file

--- src/Pages/Login/login.js
+++ src/Pages/Login/login.js
@@ -1,28 +1,42 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
-export function LoginPage(){
+import './login.css'
+
+import { BsTwitter } from 'react-icons/bs';
+
+import { FcGoogle } from 'react-icons/fc';
+
+import { BsApple } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+
+export function Login(){
+
     return(
-        <>
-        <div>
-        <div>Icon-Twitter</div>
-        <h1>Sign in to Twitter</h1>
-        <div>
-        <button>Sign in with Google</button>
+
+        <div className='login-container'>
+
+            <span className='icon'><BsTwitter/></span>
+
+            <h1>Sign in to Twitter</h1>
+
+            <button className='google-btn'><FcGoogle/>Sign in with Google</button>
+
+            <button className='apple-btn'><BsApple/>Sign in with Apple</button>
+
+            {/* <br /> */}
+            
+            <h3>or</h3>
+
+            <input type="text" placeholder='Phone, email,or username' />
+
+           <Link to="/login"> <button className='next-btn'>Next</button></Link>
+
+            <button className='forgot-btn'>Forgot password?</button>
+
+            <h3>Don't have an account? <Link to="/signUP">Sign up</Link></h3>
+
         </div>
-        <div>
-        <button>Sign in with Apple</button>
-        </div>
-        <span>or</span>
 
-        <input type="text" />
-
-        <div><button>Next</button></div>
-
-        <div><button>Forgot password?</button></div>
-
-        <h3>Don't have an account<Link>Sign up</Link></h3>
-        </div>
-        </>
     )
+
 }
--- src/Pages/SignUp/signUp.js
+++ src/Pages/SignUp/signUp.js
@@ -1,36 +1,46 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-
-
-import {signUp} from './signUp.css';
-
-export default function SignUp() {
+import './signUp.css';
+import { BsTwitter } from 'react-icons/bs';
+import { FcGoogle } from 'react-icons/fc';
+import { BsApple } from 'react-icons/bs';
+export  function SignUp() {
   return (
     
         <div className="logo">
-            <img src="./TwitterImg.png" className="logo-box"/>
+            {/* <img src="./TwitterImg.png" className="logo-box"/> */}
+
+            <span className='icons'><BsTwitter/></span>
             <h2 className="heading">Join Twitter today</h2>
 
-            
-            <button>
+            {/* <button>
             <img src="./GoogleImg.png" className='logo-box' />
             Sign Up with Google
-            </button>
-            
-            <button>
+            </button> */}
+             {/* <button>
             <img src="./AppleImg.png" className='logo-box' />
             Sign Up with Apple
-            </button>
+            </button> */}
+
+           <button className='google-btn'><FcGoogle/>Sign in with Google</button>
+
+           <button className='apple-btn'><BsApple/>Sign in with Apple</button>
+            
+           
             <hr></hr>
             <span>or</span>
-
-           <div><button>Create account</button></div>
+            
+            <Link to="/Createaccount">
+           <div>
+            <button>Create account</button>
+            </div>
+            </Link>
             
        
             <p>By signing up, you agree to the <Link>Terms of Service</Link>and <Link>Privacy Policy</Link>, including <Link>Cookie Use</Link>.</p>
          
 
-            <p>Have an account already?<Link>Log in</Link></p>
+            <p>Have an account already?<Link to="/login">Log in</Link></p>
 
         </div>
     
